e240558557392fcdc418045d22188f351902c258
jscorales <jscorales@hotmail.com>
2013-09-30 14:26:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apperture::Application.config.secret_token = 'b62dcb9cd63f6909c407b1b78d33d71c98a1ac370e1aff8a45627f8f22432d0de49bac14e8420f7becddabba129cc65b894bb17721afb6d6156af749b57892ac'
